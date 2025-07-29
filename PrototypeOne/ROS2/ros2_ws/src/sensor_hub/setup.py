b0e914a37476835a3ce9f5a27624017026293006
neckboner <kamil.kolacz05@gmail.com>
2025-07-29 21:06:08 +0200

    Update setup.py

--- PrototypeOne/ROS2/ros2_ws/src/sensor_hub/setup.py
+++ PrototypeOne/ROS2/ros2_ws/src/sensor_hub/setup.py
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pressure_publisher = py_pubsub.publisher_member_function:main',
-            'pressure_subscriber = py_pubsub.subscriber_member_function:main',
+            'pressure_publisher = sensor_hub.publisher_member_function:main',
+            'pressure_subscriber = sensor_hub.subscriber_member_function:main',
         ],
     },
 )
