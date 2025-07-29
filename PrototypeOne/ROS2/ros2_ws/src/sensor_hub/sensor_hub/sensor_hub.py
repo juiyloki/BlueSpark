8f8c20c71f06378bdfd20b09ad5fcf94b423b0ae
neckboner <kamil.kolacz05@gmail.com>
2025-07-29 21:28:49 +0200

    Update sensor_hub.py

--- PrototypeOne/ROS2/ros2_ws/src/sensor_hub/sensor_hub/sensor_hub.py
+++ PrototypeOne/ROS2/ros2_ws/src/sensor_hub/sensor_hub/sensor_hub.py
@@ -8,27 +8,27 @@ class SensorHub(Node):
 
     def __init__(self):
         super().__init__('sensor_hub')
-        self.bar30_pressure = self.create_subscription(Float32, 'bar30/pressure', self.listener_callback, 10)
-        self.bar30_temperature = self.create_subscription(Float32, 'bar30/temperature', self.listener_callback, 10)
-        self.bar30_pressure = self.create_subscription(Float32, 'bar30/pressure', self.listener_callback, 10)
-        self.bar30_pressure = self.create_subscription(Float32, 'bar30/pressure', self.listener_callback, 10)
-        self.subscription  # prevent unused variable warning
+        self.bar30_pressure = self.create_subscription(Float32, 'bar30/pressure', self.pressure_callback, 10)
+        self.bar30_temperature = self.create_subscription(Float32, 'bar30/temperature', self.temperature_callback, 10)
+        self.bar30_pressure = self.create_subscription(Float32, 'bar30/pressure', self.pressure_callback, 10)
+        self.bar30_temperature = self.create_subscription(Float32, 'bar30/temperature', self.temperature_callback, 10)
+        self.subscription
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def pressure_callback(self, msg: Float32):
+        self.get_logger().info(f'Pressure: {msg.data:.2f}')
+
+    def temperature_callback(self, msg: Float32):
+        self.get_logger().info(f'Temperature: {msg.data:.2f}')
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    'sensor_hub' = SensorHub()
+    sensor_hub = SensorHub()
 
     rclpy.spin(minimal_subscriber)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    sensor_hub.destroy_node()
     rclpy.shutdown()
 
 
